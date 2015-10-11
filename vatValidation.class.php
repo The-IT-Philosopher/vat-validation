0441de8420f6ee8f8456d92a4a3795c5b70bb0e7
André van Schoubroeck <andre@blaatschaap.be>
2015-10-11 12:02:28 +0200

    Error handling when SOAP library uses exceptions, will be changed to error states instead

--- vatValidation.class.php
+++ vatValidation.class.php
@@ -1,40 +1,44 @@
 <?php
 class vatValidation
 {
-	const WSDL = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
-	private $_client = null;
+  const WSDL = "http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl";
+  private $_client = null;
 
-	private $options  = array(
-						'debug' => false,
-						);	
-	
-	private $_valid = false;
-	private $_data = array();
-	
+  private $options  = array(
+            'debug' => false,
+            );  
+  
+  private $_valid = false;
+  private $_data = array();
+  
   private $errors;
 
-	public function __construct($options = array()) {
-		
-		foreach($options as $option => $value) {
-			$this->_options[$option] = $value;
-		}
-		
-		if(!class_exists('SoapClient')) {
-			throw new Exception('The Soap library has to be installed and enabled');
-		}
-				
-		try {
-			$this->_client = new SoapClient(self::WSDL, array('trace' => true) );
-		} catch(Exception $e) {
-			$this->trace('Vat Translation Error', $e->getMessage());
-		}
-	}
+  public function __construct($options = array()) {
+    
+    foreach($options as $option => $value) {
+      $this->_options[$option] = $value;
+    }
+    
+    if(!class_exists('SoapClient')) {
+      // throw new Exception('The Soap library has to be installed and enabled');
+      // for now ... as the manager project does not yet use exceptions for 
+      // error handling
+      $errors   = array();
+      $errors[] = new Exception('The Soap library has to be installed and enabled');
+    }
+        
+    try {
+      $this->_client = new SoapClient(self::WSDL, array('trace' => true) );
+    } catch(Exception $e) {
+      $this->trace('Vat Translation Error', $e->getMessage());
+    }
+  }
 
-	public function check($fullVatNumber) {
+  public function check($fullVatNumber) {
     // TODO: 
     // Some countries have planned downtime at a specific time
     // What is the response from the service when this happens?
-
+    if (!$this->_client) return NULL;
     try {
       $countryCode = substr($fullVatNumber,0,2);
       $vatNumber   = substr($fullVatNumber,2);
@@ -46,39 +50,39 @@ class vatValidation
     }
   }
 
-	private function checkSplitted($countryCode, $vatNumber) {
+  private function checkSplitted($countryCode, $vatNumber) {
 
-		$rs = $this->_client->checkVat( array('countryCode' => $countryCode, 'vatNumber' => $vatNumber) );
+    $rs = $this->_client->checkVat( array('countryCode' => $countryCode, 'vatNumber' => $vatNumber) );
 
-		if($this->isDebug()) {
-			$this->trace('Web Service result', $this->_client->__getLastResponse());	
-		}
+    if($this->isDebug()) {
+      $this->trace('Web Service result', $this->_client->__getLastResponse());  
+    }
 
-		if($rs->valid) {
-			$this->_valid = true;
-			$this->_data = array(
-									'name' => 			$rs->name,
-									'address' => 		$rs->address,
-								);
-			return true;
-		} else {
-			$this->_valid = false;
-			$this->_data = array();
-		    return false;
-		}
-	}
+    if($rs->valid) {
+      $this->_valid = true;
+      $this->_data = array(
+                  'name' =>       $rs->name,
+                  'address' =>     $rs->address,
+                );
+      return true;
+    } else {
+      $this->_valid = false;
+      $this->_data = array();
+        return false;
+    }
+  }
 
-	public function isValid() {
-		return $this->_valid;
-	}
-	
-	public function getName() {
-		return $this->_data['name'];
-	}
-	
-	public function getAddress() {
-		return $this->_data['address'];
-	}
+  public function isValid() {
+    return $this->_valid;
+  }
+  
+  public function getName() {
+    return $this->_data['name'];
+  }
+  
+  public function getAddress() {
+    return $this->_data['address'];
+  }
 
   public function getData() {
     $data = array();
@@ -86,32 +90,32 @@ class vatValidation
     $data['address']           = $this->_data['address'];
     return $data;
   }
-	
-	public function isDebug() {
-		return ($this->_options['debug'] === true);
-	}
-	private function trace($title,$body) {
-		echo '<h2>TRACE: '.$title.'</h2><pre>'. wordwrap(htmlentities($body)).'</pre>';
-	}
-	private function cleanUpString($string) {
+  
+  public function isDebug() {
+    return ($this->_options['debug'] === true);
+  }
+  private function trace($title,$body) {
+    echo '<h2>TRACE: '.$title.'</h2><pre>'. wordwrap(htmlentities($body)).'</pre>';
+  }
+  private function cleanUpString($string) {
         for($i=0;$i<100;$i++)
         {               
             $newString = str_replace("  "," ",$string);
             if($newString === $string) {
-            	break;
+              break;
             } else {
-            	$string = $newString;
-			}
+              $string = $newString;
+      }
         }
                         
         $newString = "";
         $words = explode(" ",$string);
         foreach($words as $k=>$w)
         {                       
-           	$newString .= ucfirst(strtolower($w))." "; 
+             $newString .= ucfirst(strtolower($w))." "; 
         }                
         return $newString;
-	}
+  }
 }
 
 ?>
